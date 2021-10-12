c50422bf9d4be9c60341b0b043d28b5e9c69fcf9
Jeremy <jeremyeparker@yahoo.com>
2021-10-12 18:31:15 -0400

    player class

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,2 +1,19 @@
-package com.company;public class Player {
+package com.company;
+
+import java.util.ArrayList;
+
+public class Player {
+
+    ArrayList <Card> hand = new ArrayList<>();
+
+    public void printHand(){
+        for(Card c: hand){
+            System.out.println(c);
+        }
+    }
+
+    public Card discard(){
+
+        return hand.remove(hand.size() - 1);
+    }
 }
