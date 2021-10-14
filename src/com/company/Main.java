379d28c2864c96009e55147703a6ad9bc21a2704
Jeremy <jeremyeparker@yahoo.com>
2021-10-13 23:49:30 -0400

    discard deck ui

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -10,6 +11,7 @@ public class Main {
         Player p = new Player();
 
         Deck d = new Deck();
+        ArrayList<Card> discard = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -19,6 +21,11 @@ public class Main {
         System.out.println("---");
 
         while(true){
+
+            if(discard.size() > 0){
+                System.out.println("Pick a card from the discard?");
+            }
+
             System.out.println("Would you like to draw a card?");
             System.out.println("Yes (1) or No (2)");
             int i = scanner.nextInt();
@@ -30,13 +37,15 @@ public class Main {
                 System.out.println("Yes (1) or No (2)");
                 int j = scanner.nextInt();
                 if(j != 2){
-                    d.add(p.discard());
+                    discard.add(p.discard());
                     p.printHand();
                 }else{
                     System.out.println("---");
                     System.out.println("---");
                     System.out.println("---");
-
+                    for(Card c: discard){
+                        System.out.println(c.display());
+                    }
                     System.out.println("---");
                     break;
                 }
