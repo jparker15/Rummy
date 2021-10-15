3c685739390c1d43a6a9223021bee270f501e1c9
Jeremy <jeremyeparker@yahoo.com>
2021-10-14 20:34:38 -0400

    commented out to add new classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,56 +1,61 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        Player p = new Player();
-
-        Deck d = new Deck();
-        ArrayList<Card> discard = new ArrayList<>();
-
-        Scanner scanner = new Scanner(System.in);
-
-        d.shuffle();
-        System.out.println("Welcome to Rummy");
-        System.out.println("---");
-        System.out.println("---");
-
-        while(true){
-
-            if(discard.size() > 0){
-                System.out.println("Pick a card from the discard?");
-            }
-
-            System.out.println("Would you like to draw a card?");
-            System.out.println("Yes (1) or No (2)");
-            int i = scanner.nextInt();
-            if(i != 2){
-                p.hand.add(d.draw());
-                p.printHand();
-            }else{
-                System.out.println("Would you like to discard a card?");
-                System.out.println("Yes (1) or No (2)");
-                int j = scanner.nextInt();
-                if(j != 2){
-                    discard.add(p.discard());
-                    p.printHand();
-                }else{
-                    System.out.println("---");
-                    System.out.println("---");
-                    System.out.println("---");
-                    for(Card c: discard){
-                        System.out.println(c.display());
-                    }
-                    System.out.println("---");
-                    break;
-                }
-            }
-        }
+//
+//        Player one = new Player();
+//        Player two = new Player();
+//
+//        Deck d = new Deck();
+//        ArrayList<Card> discard = new ArrayList<>();
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        d.shuffle();
+//        System.out.println("Welcome to Rummy");
+//        System.out.println("---");
+//        System.out.println("---\n");
+//
+//        while(true){
+//
+//            System.out.println("Would you like to draw a card?");
+//            System.out.println("Yes (1) or No (2)");
+//            int i = scanner.nextInt();
+//            if(i != 2){
+//                p.hand.add(d.draw());
+//                p.printHand();
+//            }else {
+//                System.out.println("Would you like to discard a card?");
+//                System.out.println("Yes (1) or No (2)");
+//                int j = scanner.nextInt();
+//                if (j != 2) {
+//                    discard.add(p.discard());
+//                    p.printHand();
+//                }
+//            }
+//
+//            if(discard.size() > 0){
+//                System.out.println("Pick a card from the discard?");
+//
+//                for(int k = 0; k < discard.size(); k++){
+//                    System.out.println(discard.get(k) + "(" + k + ")");
+//                }
+//            }
+//
+////                }else{
+////                    System.out.println("---");
+////                    System.out.println("---");
+////                    System.out.println("---");
+////                    for(Card c: discard){
+////                        System.out.println(c.display());
+////                    }
+////                    System.out.println("---");
+////                    break;
+////                }
+////            }
+//        }
 
 
     }
