115659b14522cde5bb7d8ecb0283de4f7b24e409
Jeremy <jeremyeparker@yahoo.com>
2021-10-14 20:34:18 -0400

    add interface methods

--- src/com/company/Game/actor/Player.java
+++ src/com/company/Game/actor/Player.java
@@ -1,12 +1,19 @@
-package com.company.actor;
+package com.company.Game.actor;
 
-import com.company.Card;
+import com.company.Game.deck.Card;
+import com.company.Game.rummy.Actor;
+import com.company.Game.rummy.Hand;
+import com.company.Utils.Console;
 
 import java.util.ArrayList;
 
-public class Player {
-    private
+public class Player implements Actor {
+    private final String name;
+    private int actionsCounts;
     ArrayList<Card> hand = new ArrayList<>();
+
+    public Player(String name) {this.name = name;
+    }
 
     public void printHand(){
         for(Card c: hand){
@@ -17,5 +24,23 @@ public class Player {
     public Card discard(){
 
         return hand.remove(hand.size() - 1);
+    }
+
+    private String getAvailableActions(Hand hand){
+        actionsCounts = 2;
+        return "0. Quit\n1. Draw\n2. Discard\n3. Draw from Discards";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public byte getAction(Hand hand) {
+        System.out.println(hand.displayHand());
+        System.out.println(hand.getValue());
+
+        return (byte) Console.getInt(getAvailableActions(hand),0,actionsCounts,"invalid action");
     }
 }
