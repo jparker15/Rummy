163c30e3d85fbd7b34783d933d9dd382a1966f54
Jeremy <jeremyeparker@yahoo.com>
2021-10-14 20:35:32 -0400

    change from class to interface

--- src/com/company/Game/deck/Deck.java
+++ src/com/company/Game/deck/Deck.java
@@ -1,48 +1,7 @@
-package com.company;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class Deck {
-
-    private final List<Card> cards = new ArrayList<>();
-
-    public final String[] SUITS ={
-            "CLUBS", "HEARTS", "DIAMONDS", "SPADES"
-    };
-
-    public final int[] VALUES = {
-            1,2,3,4,5,6,7,8,9,10,11,12,13
-    };
-
-    public Deck(){
-        for (String s:SUITS) {
-            for(int v: VALUES){
-                cards.add(new Card(s,v));
-            }
-        }
-    }
-
-    public void printDeck(){
-        for(Card c: cards){
-            System.out.println(c);
-        }
-    }
-
-    public Card deal(){
-        //deals bottom card
-        return cards.remove(cards.size() - 1);
-    }
+package com.company.Game.deck;
 
 
-    public void shuffle() {
-        Collections.shuffle(cards);
-    }
-
-    public Card draw() {
-        return cards.remove(cards.size() - 1);
-    }
-
-    public void add(Card card){ cards.add(0, card);}
+public interface Deck {
+    void shuffle();
+    Card draw();
 }
