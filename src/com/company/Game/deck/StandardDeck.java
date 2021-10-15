34791465dd200137ed4afc9d2cf33f27cd6be1b2
Jeremy <jeremyeparker@yahoo.com>
2021-10-14 20:36:22 -0400

    standard deck for rummy

--- src/com/company/Game/deck/StandardDeck.java
+++ src/com/company/Game/deck/StandardDeck.java
@@ -1,2 +1,33 @@
-package com.company.Game.deck;public class StandardDeck {
+package com.company.Game.deck;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class StandardDeck implements Deck{
+
+    private final List<Card> cards = new ArrayList<>();
+    public final String[] SUITS ={
+            "CLUBS", "HEARTS", "DIAMONDS", "SPADES"
+    };
+    public final int[] VALUES = {
+            1,2,3,4,5,6,7,8,9,10,11,12,13
+    };
+
+    public StandardDeck(){
+        for (String s:SUITS) {
+            for(int v: VALUES){
+                cards.add(new Card(s,v));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card draw() {
+        return cards.remove(cards.size() - 1);
+    }
+
 }
