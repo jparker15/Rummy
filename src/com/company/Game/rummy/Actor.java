dcc1eed14cd2628572daa3cb4545e48482e80ec0
Jeremy <jeremyeparker@yahoo.com>
2021-10-14 20:37:08 -0400

    input actions for actor

--- src/com/company/Game/rummy/Actor.java
+++ src/com/company/Game/rummy/Actor.java
@@ -1,2 +1,10 @@
-package com.company.Game.rummy;public interface Actor {
+package com.company.Game.rummy;
+
+public interface Actor {
+    byte DRAW = 1;
+    byte DISCARD = 2;
+    byte QUIT = 3;
+
+    String getName();
+    byte getAction(Hand hand);
 }
