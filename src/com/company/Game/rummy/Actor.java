1028f98c696c18e81a1ceefd3a92cc0d80c1e60f
Jeremy <jeremyeparker@yahoo.com>
2021-10-25 19:04:16 -0400

    change/add fields

--- src/com/company/Game/rummy/Actor.java
+++ src/com/company/Game/rummy/Actor.java
@@ -2,8 +2,11 @@ package com.company.Game.rummy;
 
 public interface Actor {
     byte DRAW = 1;
-    byte DISCARD = 2;
-    byte QUIT = 3;
+    byte ALTDRAW = 2;
+    byte KNOCK = 3;
+    byte VALUESORT = 4;
+    byte SUITSORT = 5;
+    byte QUIT = 0;
 
     String getName();
     byte getAction(Hand hand);
