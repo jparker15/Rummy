a99f5d7543c4c1caa266a00b3c2a686238051673
Jeremy <jeremyeparker@yahoo.com>
2021-10-25 19:03:47 -0400

    implement Deck interface

--- src/com/company/Game/deck/DiscardDeck.java
+++ src/com/company/Game/deck/DiscardDeck.java
@@ -3,7 +3,35 @@ package com.company.Game.deck;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DiscardDeck {
+public class DiscardDeck implements Deck {
     private final List<Card> cards = new ArrayList<>();
 
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    public int getSize(){return cards.size();}
+
+    public String displayDeck(){
+        StringBuilder output = new StringBuilder();
+        for(Card card: cards){
+            output.append(card.display()).append(" \n");
+        }
+        return output.toString().trim();
+    }
+
+    public Card draw(int index){
+        return cards.remove(index);
+    }
+
+    @Override
+    public void shuffle() {
+
+    }
+
+    @Override
+    public Card draw() {
+
+        return null;
+    }
 }
